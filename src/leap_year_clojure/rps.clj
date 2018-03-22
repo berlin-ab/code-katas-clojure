2907f24a967db4b6ea195ed57338dc5c2c412d51
Adam Berlin <adamberlin@Adams-MBP.fios-router.home>
2018-03-21 23:07:41 -0400

    Adds RPS kata.

---
+++ src/leap_year_clojure/rps.clj
@@ -0,0 +1,48 @@
+(ns leap-year-clojure.rps)
+
+(def rock :rock)
+(def paper :paper)
+(def scissors :scissors)
+(def player-one-wins :player-one-wins)
+(def player-two-wins :player-two-wins)
+(def tie-game :tie-game)
+(def invalid-game :invalid-game)
+
+(defn- player-one-beats-player-two [throw-one throw-two]
+  (or
+    (and
+      (= throw-one rock)
+      (= throw-two scissors))
+    (and
+      (= throw-one paper)
+      (= throw-two rock))
+    (and
+      (= throw-one scissors)
+      (= throw-two paper))))
+
+(defn- throws-are-equal [throw-one throw-two]
+  (= throw-one throw-two))
+
+(defn- valid-throw [throw]
+  (or
+    (= throw rock)
+    (= throw paper)
+    (= throw scissors)))
+
+(defn- are-invalid-throws [throw-one throw-two]
+  (not
+    (and
+      (valid-throw throw-one)
+      (valid-throw throw-two))))
+
+(defn play [throw-one throw-two]
+  (cond
+    (are-invalid-throws throw-one throw-two)
+      invalid-game
+    (throws-are-equal throw-one throw-two)
+      tie-game
+    (player-one-beats-player-two throw-one throw-two)
+      player-one-wins
+    :else
+      player-two-wins))
+
---
+++ test/leap_year_clojure/rps_test.clj
@@ -0,0 +1,16 @@
+(ns leap-year-clojure.rps-test
+  (:require [clojure.test :refer :all])
+  (:require [leap-year-clojure.rps :refer :all]))
+
+(deftest rock-paper-scissors
+  (testing "throws"
+    (is (= player-one-wins (play rock scissors))))
+    (is (= player-one-wins (play scissors paper)))
+    (is (= player-one-wins (play paper rock)))
+    (is (= player-two-wins (play rock paper)))
+    (is (= player-two-wins (play scissors rock)))
+    (is (= player-two-wins (play paper scissors)))
+    (is (= tie-game (play rock rock)))
+    (is (= tie-game (play paper paper)))
+    (is (= tie-game (play scissors scissors)))
+    (is (= invalid-game (play scissors :something-else))))
